c83877ef44106d1f112194440941783137ea12b2
bevinhuynh <bevinh@uci.edu>
2024-12-15 20:32:36 -0800

    Refined searchbar and added svg, fixed background color issues due to Bootstrap import

--- src/AboutHeader.tsx
+++ src/AboutHeader.tsx
@@ -1,4 +1,4 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 
 function AboutHeader() {
     return (
@@ -6,7 +6,8 @@ function AboutHeader() {
             <div>
                 <div className='container'>
                     <div className='row'>                       
-                            <a href='https://www.google.com/'id='about-link'>Hello</a>                                          
+                            <a href='https://www.google.com/'id='about-link'>Hello</a>  
+                            <a href='https://github.com/TaylorTraan/SearchEngine' id='github-link'>Github</a>                                  
                     </div>                   
                 </div>
             </div>
--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
 // import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='container-fluid'>
+        <div className='row'>
+          <a>
+            <img src="../src/assets/anteater.svg" className="anteater" alt="Anteater logo" id="anteater" />
+          </a>
+        </div>
+        <div className='row' id='title'>
+          <h1>Zoogle</h1>
+        </div>
+        <div className='row' id='searchbar'>
+          <form id='search-bar-form'>
+            <input id='search-bar' placeholder='Search Zoogle'></input>
+            <img id='searchimg' src='../src/assets/searchimg.svg'></img>
+          </form>
+        </div>
+        <div className='row' id="buttons">
+          <div className='col' id='search-button'>
+            <button>Zoogle Search</button>
+          </div>
+          <div className='col' id='lucky-button'>
+            <button>I'm feeling lucky</button> 
+          </div>
+        </div>
+       
       </div>
-      <h1>Vite + React</h1>
+
+
+      {/* <h1>Zoogle</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -29,7 +46,7 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
+      </p> */}
     </>
   )
 }
