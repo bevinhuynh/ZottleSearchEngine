83ad21739da4ef66f212e34197c9e71d0581c9ed
bevinhuynh <bevinh@uci.edu>
2024-12-18 01:22:36 -0800

    Added header to search query page, added links and tf-idf scores stacked vertically

--- src/components/QueryDisplay.tsx
+++ src/components/QueryDisplay.tsx
@@ -3,63 +3,17 @@ import "../App.css"
 interface QueryDisplayProps {
     url: string;
     tfidf: number;
+    index_num: number;
 }
 
-const QueryDisplay: React.FC<QueryDisplayProps> = ({url, tfidf}) =>  {
-    console.log(url)
+const QueryDisplay: React.FC<QueryDisplayProps> = ({url, tfidf, index_num}) =>  {
     return (
         <div>
-            <h1 id="url">{url}</h1>
+            <a id="url-link" href={url}>{index_num}.&nbsp;&nbsp;{url}</a>
+            <p id="tf-idf-score"> - TF-IDF score: {tfidf}</p>
         </div>
     )
     
 }
 
 export default QueryDisplay;
-
-
-#search-header {
-    /* background-color: black; */
-    position: relative;
-    margin-bottom: 43.5em;
-    /* margin-right: 78em; */
-    width: 90em;
-
-}
-
-#zottle-logo {
-    font-family: sans-serif;
-    letter-spacing: 1px;
-    font-size: 2.3em;
-    display: flex;
-    margin-left: 1em;
-    margin-top: 0.5em;
-    width: fit-content;
-  }
-
-  .anteater {
-    display: flex;
-    height: 6.5em;
-    will-change: filter;
-    transition: filter 300ms;
-    /* background-color: white; */
-    margin-left: 10em;
-    margin-top: -5em;
-  }
-  
-.letter.z { color: #4285F4; }   /* Z (Blue) */
-.letter.o1 { color: #EA4335; }  /* First o (Red) */
-.letter.t1 { color: #FBBC05; }  /* Second o (Yellow) */
-.letter.t2 { color: #4285F4; }   /* g (Blue) */
-.letter.l { color: #34A853; }   /* l (Green) */
-.letter.e { color: #EA4335; }   /* e (Red) */
-
-
-#search-header {
-    /* background-color: black; */
-    position: relative;
-    margin-bottom: 43.5em;
-    /* margin-right: 78em; */
-    width: 90em;
-
-}
--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -5,21 +5,30 @@ import QueryDisplay from "../components/QueryDisplay";
 function SearchPage() {
   const results = useLocation();  // Extract search results from useLocation
   const urlArray = results.state.searchResults[0]
-  
+  console.log(urlArray)
   return (
     <>
-     <SearchHeader/>
+
+     <div className="container" id="header-container">
+      <div className="row">
+        <SearchHeader/>
+      </div>
+     </div>
+     <div id="total-container">
+      <div className="container" id="url-container">
+        {urlArray.length > 0 ? (
+          urlArray.map((result: any, index: number) => (
+            <div className="row" key={index} id="url-row"> 
+                <QueryDisplay url={result[0]} tfidf={result[1].toFixed(2)} index_num={index+1} />
+            </div>
+          ))
+        ) : (
+          <h1>No results found.</h1>
+        )}
+      </div>
+    </div>
     </>
   );
 }
 
 export default SearchPage;
- {/* <div className="container">
-        {urlArray.length > 0 ? (
-          urlArray.map((result: any, index: number) => (
-            <QueryDisplay key={index} url={result[0]} tfidf={result[1]} />
-          ))
-        ) : (
-          <h1>No results found.</h1>
-        )}
-      </div> */}
